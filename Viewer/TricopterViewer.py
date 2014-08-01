c299a5cd69770264f58641042971d87219720a60
etienne <etienne.servais@mines-paristech.fr>
2014-08-01 15:27:29 +0200

    Code refactoring. function update(self) rewritten. Should be easier to understand and quicker

--- Viewer/TricopterViewer.py
+++ Viewer/TricopterViewer.py
@@ -15,94 +15,66 @@ import argparse
 class TricopterData:
   # constr
   def __init__(self, args):
-    #gets the various params
-    strPort = args.strPort
-    baudrate = args.baudrate
-    maxLen = args.maxLen
-    n32 = args.n32
-    n16 = args.n16
-    n8 = args.n8
-    nf = args.nf
-
-    print('reading from serial port %s...' % strPort)
+    print('reading from serial port %s...' % args.strPort)
     # open serial port
-    self.ser = serial.Serial(strPort, baudrate)
-    self.n32 = n32
-    self.d32 = [[] for x in xrange(n32)]
-    self.n16 = n16
-    self.d16 = [[] for x in xrange(n16)]
-    self.n8 = n8
-    self.d8 = [[] for x in xrange(n8)]
-    self.nf = nf
-    self.df = [[] for x in xrange(nf)]
+    self.ser = serial.Serial(args.strPort, args.baudrate)
+    self.n32 = args.n32
+    self.d32 = [[] for x in xrange(args.n32)]
+    self.n16 = args.n16
+    self.d16 = [[] for x in xrange(args.n16)]
+    self.n8 = args.n8
+    self.d8 = [[] for x in xrange(args.n8)]
+    self.nf = args.nf
+    self.df = [[] for x in xrange(args.nf)]
 
-    #self.ax = deque([0.0]*maxLen)
-    #self.ay = deque([0.0]*maxLen)
-    self.maxLen = maxLen
     #This is the header of the packet, it's where it starts
     self.header=args.header
     self.headerLength = len(self.header)
     self.buffer=""
+    self.complete = False # is turned to true once a header has been complete.
     self.packet=""
     self.newData=False
     
   #this function parse the packet (complete) in the data
   def parse(self):
-    offset = self.headerLength 
     for i in range (0, self.n32):
-      if (len(self.d32[i]) == self.maxLen):
-        del self.d32[i][0]
       (self.d32[i]).append(  struct.unpack('<i',self.packet[offset+i*5+1:offset+i*5+5])[0])
       #print((self.d32[i])[self.elements],end="\t")
-    offset = self.headerLength + 5*self.n32 
+    offset = 5*self.n32 
     for i in range (0, self.n16):
-      if (len(self.d16[i]) == self.maxLen):
-        del self.d16[i][0]
       (self.d16[i]).append(struct.unpack('<h',self.packet[offset+i*3+1:offset+i*3+3])[0])
       #print((self.d16[i])[self.elements],end="\t")
-    offset = self.headerLength + 5*self.n32 + 3*self.n16 
+    offset = 5*self.n32 + 3*self.n16 
     for i in range (0, self.n8):
-      if (len(self.d8[i]) == self.maxLen):
-        del self.d8[i][0]
       (self.d8[i]).append(struct.unpack('<c',self.packet[offset+i*2+1:offset+i*2+2])[0])
       #print((self.d8[i])[self.elements],end="\t")
-    offset = self.headerLength + 5*self.n32 + 3*self.n16 + 2*self.n8
+    offset = 5*self.n32 + 3*self.n16 + 2*self.n8
     for i in range (0, self.nf):
-      if (len(self.df[i]) == self.maxLen):
-        del self.df[i][0]
       self.df[i].append(struct.unpack('<f',self.packet[offset+i*5+1:offset+i*5+5])[0])
       #print((self.df[i]),end="\t")
-    #print("")
-    #print(self.elements)
+    self.packet = ""
 
   # update plot
   #def update(self, frameNum, a0, a1):
   def update(self):
     try:
-      letter = self.ser.read()
-      print(letter.encode('hex'))
-      self.buffer +=  letter
-      if (len(self.buffer) == self.headerLength):
-        self.buffer = self.buffer[1:]
-      if (self.buffer.encode('hex') == self.header):
-        print(self.packet.encode('hex'))
-        #In some cases, the packet might not be complete. 
-        #We check this 
-        if( len(self.packet) == 4 + 5*self.n32 + 3*self.n16 + 2*self.n8 + 5*self.nf ):
-          #the packet is complete
-          #We should decode it:
-          self.newData=True
-          self.parse()
-        else:
-          print("Packet not complete !",end="");
-          print("Length=",end="");
-          print(len(self.packet),end="");
-          print(" expected ",end="");
-          print(4 + 5*self.n32 + 3*self.n16 + 2*self.n8 + 5*self.nf );
-          print(self.packet.encode('hex'))
-        self.packet = self.buffer[0]
+      if (self.headerComplete):
+        # The header is complete, we read 
+        # the content of the packet
+        self.packet += self.ser.read(5*self.n32 + 3*self.n16 + 2*self.n8 + 5*self.nf)
+        #The packet is complete (if no error...) 
+        #Parse it !
+        self.parse()
+        self.newData=True
+        self.headerComplete = False
       else:
-        self.packet += self.buffer[0]
+        #We haven't reached a header yet, 
+        # let roll over the incoming bytes
+        self.buffer +=  self.ser.read()
+        if (len(self.buffer) > self.headerLength):
+          self.buffer = self.buffer[1:]
+        if (self.buffer.encode('hex') == self.header):
+          self.headerComplete = False
     except KeyboardInterrupt:
         print('exiting')
 
@@ -111,8 +83,6 @@ class TricopterData:
     # close serial
     self.ser.flush()
     self.ser.close()    
-
-
 
 # main() function
 def main():
